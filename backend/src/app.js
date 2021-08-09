f514c698f3d8652df9055f8edb46443b6a3ba845
LucasGSilva21 <lucasgsilva2102@gmail.com>
2021-08-09 13:06:07 -0300

    config routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const { routes } = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome!');
-});
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
 module.exports = { app };
