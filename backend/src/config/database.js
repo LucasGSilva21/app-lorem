65c905169c3368cfc891545fd804a4fa3d62acf3
LucasGSilva21 <lucasgsilva2102@gmail.com>
2021-08-13 17:00:54 -0300

    config database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,23 +1,25 @@
+require('dotenv/config');
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.USER_DB,
+    "password": process.env.PASSWORD_DB,
+    "database": process.env.NAME_DB,
+    "host": process.env.HOST_DB,
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
